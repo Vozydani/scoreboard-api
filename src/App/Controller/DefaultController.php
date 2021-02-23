0334271cae327e5c517d1d85704ccf03acf0c0b9
Yurii Antonchyk <yurii.antonchyk@onthe.io>
2021-02-23 21:48:47 +0200

    Add DefaultController

---
+++ src/App/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Class DefaultController
+ * @package App\Controller
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json(['status' => 'ok']);
+    }
+}
